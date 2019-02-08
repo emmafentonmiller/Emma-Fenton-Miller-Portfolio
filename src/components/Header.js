1c1f68579bfb9d72bfd07e48090585b98bf14562
emmafentonmiller <46136505+emmafentonmiller@users.noreply.github.com>
2019-02-08 11:43:33 -0600

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -67,7 +67,7 @@ const Header = ({ name, location, socialMedia }) => (
       <SocialMedia>
         {socialMedia.map(social => (
           <a key={social.name} href={social.url} rel="noopener noreferrer" target="_blank">
-            <FaInstagram size={42} />
+            <FaInstagram size={35} />
           </a>
         ))}
         
