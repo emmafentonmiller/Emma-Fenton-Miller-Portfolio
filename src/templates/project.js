027712710f27acfa7f4b9a119a8381e5763c419c
emmafentonmiller <46136505+emmafentonmiller@users.noreply.github.com>
2020-04-20 14:17:02 -0500

    Update project.js

--- src/templates/project.js
+++ src/templates/project.js
@@ -84,7 +84,7 @@ export const pageQuery = graphql`
       edges {
         node {
           childImageSharp {
-            fluid(maxWidth: 1600, quality: 90, traceSVG: { color: "#ffffff" }) {
+            fluid(maxWidth: 1200, quality: 90, traceSVG: { color: "#ffffff" }) {
               ...GatsbyImageSharpFluid_withWebp_tracedSVG
             }
           }
