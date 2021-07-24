58ce7acd4c7893a56e4e09457bbc092265e62e11
emmafentonmiller <46136505+emmafentonmiller@users.noreply.github.com>
2021-07-24 16:08:25 -0500

    Update project.js

--- src/templates/project.js
+++ src/templates/project.js
@@ -97,7 +97,7 @@ export const pageQuery = graphql`
       frontmatter {
         cover {
           childImageSharp {
-            fluid(maxWidth: 1600, quality: 90, traceSVG: { color: "#ffffff" }) {
+            fluid(maxWidth: 1200, quality: 90, traceSVG: { color: "#ffffff" }) {
               ...GatsbyImageSharpFluid_withWebp_tracedSVG
             }
             resize(width: 800) {
